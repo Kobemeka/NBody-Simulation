4a2dbd416b96a97a369d4eb042c9b7e96d9042f3
Kubilay <meneskaytemiz@hotmail.com>
2022-01-17 00:53:04 +0000

    Legend
    Bodies have names and they are labeled on the plot.

--- nbody.py
+++ nbody.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -44,11 +45,11 @@ class Vector:
         return f"Vector({self.x}, {self.y})"
 
 class Body:
-    def __init__(self,position:Vector,velocity:Vector,mass:float) -> None:
+    def __init__(self,position:Vector,velocity:Vector,mass:float,name="Body") -> None:
         self.position = position
         self.velocity = velocity
         self.mass = mass
-
+        self.name = name
     def actingAcceleration(self,objects,exclude):
         
         return sum([acceleration(o.position - self.position,o.mass) for i,o in enumerate(objects) if i != exclude])
@@ -59,7 +60,7 @@ class Body:
         return self.position
 
 def createRandomNBody(n):
-    return np.array([Body(Vector(1e7*random.randint(-5,5),1e7*random.randint(-5,5)),Vector(random.randint(-50,50),random.randint(-50,50)),1e24*random.randint(1,10)) for i in range(n)])
+    return np.array([Body(Vector(1e7*random.randint(-5,5),1e7*random.randint(-5,5)),Vector(random.randint(-50,50),random.randint(-50,50)),1e24*random.randint(1,10),f"Body-{i}") for i in range(n)])
 
 def distance(a,b):
     return a.position - b.position
@@ -75,7 +76,7 @@ def animate(bodies,time_step,total_time,boundaries:tuple):
     fig,ax = plt.subplots()
     positionsx = [[] for _ in range(len(bodies))]
     positionsy = [[] for _ in range(len(bodies))]
-    animPlot = [plt.plot([],[],".-")[0] for _ in range(len(bodies))]
+    animPlot = [plt.plot([],[],".-",label=f"{bodies[i].name}")[0] for i in range(len(bodies))]
 
     def init():
         ax.set_xlim(-boundaries[0],boundaries[0])
@@ -98,5 +99,5 @@ def animate(bodies,time_step,total_time,boundaries:tuple):
     new_positions = [[b.changePosition(bodies,time_step,i) for i,b in enumerate(bodies)] for t in range(total_time)]
 
     ani = FuncAnimation(fig, update, frames=new_positions,init_func=init,interval=50)
-
+    plt.legend()
     plt.show()
